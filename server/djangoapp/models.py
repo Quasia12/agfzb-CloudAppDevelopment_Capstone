82e990e87cbc75d12cb30e879df4f0a140731ec7
Shaquasia Barksdale <quasia1288@gmail.com>
2022-12-19 12:28:52 -0500

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -11,6 +11,33 @@ class CarMake(models.Model):
     def __str__(self):
         return "Name: " + self.name
 
+class CarModel(models.Model):
+    id = models.IntegerField(default=1,primary_key=True)
+    name = models.CharField(null=False, max_length=100, default='Car')
+   
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    MINIVAN = 'Minivan'
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (MINIVAN, 'Minivan')
+    ]
+
+    type = models.CharField(
+        null=False,
+        max_length=50,
+        choices=CAR_TYPES,
+        default=SEDAN
+    )
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    year = models.DateField(default=now)
+
+    def __str__(self):
+        return "Name: " + self.name    
+
 class CarDealer:
     def __init__(self, address, city, full_name, id, lat, long, st, zip, short_name):
         # Dealer address
